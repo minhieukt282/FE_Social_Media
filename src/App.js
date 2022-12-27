97e5ac987ceabd29c65f9d03f76cec252fc0575b
minhieukt <minhieukt@gmail.com>
2022-12-27 10:00:32 +0700

    add file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-
+        <div >
+            <Routes>
+                <Route path="/">
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="register" element={<Register/>}/>
+                    <Route path="" element={<Home/>}/>
+                    <Route path=":username">
+                        {/*<Route path=":userId" element={<Profile/>}/>*/}
+                        {/*<Route path=":userId/edit" element={<EditProfile/>}/>*/}
+                    </Route>
+                </Route>
+            </Routes>
         </div>
     );
 }
