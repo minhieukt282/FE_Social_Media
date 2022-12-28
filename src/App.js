b4935d791bb4f5f87fb741f0fc4c705865589876
Cikie <daoanh123123@gmail.com>
2022-12-28 16:36:48 +0700

    done register

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import {Route, Routes} from "react-router-dom";
-// import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {getPost} from "./services/postServices";
-import {login, loginWed} from "./services/loginServices";
 
 function App() {
-    // const dispatch = useDispatch();
-    // const store = useSelector(state => {
-    //     return state
-    // });
-    // console.log(store)
-    // useEffect(()=>{
-    //     dispatch(getPost());
-    // },[])
-    // useEffect(()=>{
-    //     dispatch(loginWed({
-    //         username:'1',
-    //         password:'1'
-    //     }));
-    // },[])
     return (
         <div >
             <Routes>
                 <Route path="/">
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/>
-                    {/*<Route path="" element={<Home/>}/>*/}
                     <Route path="/profile" element={<Profile/>}/>
                 </Route>
             </Routes>
