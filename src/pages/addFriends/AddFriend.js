06b995e93def5e719641ccb672e95aa8331110e9
hale113 <lehanhim97@gmail.com>
2023-01-12 08:53:57 +0700

    12/1

---
+++ src/pages/addFriends/AddFriend.js
@@ -0,0 +1,25 @@
+import MultiActionAreaCard from "../../components/cardFriend/card";
+import React, {useEffect} from "react";
+
+export default function AddFriend({socket}) {
+    useEffect(() => {
+        if (socket != null) {
+            socket.emit("refresh", {
+                accountId: JSON.parse(localStorage.getItem("accountId"))
+            })
+            socket.emit("findUser", {
+                accountId: JSON.parse(localStorage.getItem("accountId"))
+            })
+        }
+    }, [socket])
+
+    return (
+        <>
+            <h2 style={{paddingTop: 20, paddingLeft: 10}}>Friends request</h2>
+            <hr/>
+            <div className="row">
+                <MultiActionAreaCard socket={socket}></MultiActionAreaCard>
+            </div>
+        </div>
+    )
+}
